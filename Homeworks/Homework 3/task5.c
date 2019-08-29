7a2af4e1bb72fd6249dda21149dcc2efc034845c
Esaú Peralta <esau.opr@gmail.com>
2019-08-29 16:32:04 -0500

    feat(homeworks): Finish homework for linked lists

--- Homeworks/Homework 3/task5.c
+++ Homeworks/Homework 3/task5.c
@@ -10,22 +10,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 typedef struct node {
     int value;
     struct  node * next;
 } Node;
 
 
-void print(Node * node) {
-    if (node == NULL) {
-        puts("");
-        return;
-    };
-    printf("%d ", node->value);
-    print(node->next);
-}
-
-
+/*
+    Insert and allocate memory for a
+    new node at the end of the list
+*/
 void add_element(Node ** node, int num) {
     if (*node == NULL) {
         (*node) = (Node *)malloc(sizeof(Node));
@@ -38,6 +33,18 @@ void add_element(Node ** node, int num) {
 }
 
 
+/* Print the list elements */
+void print(Node * node) {
+    if (node == NULL) {
+        puts("");
+        return;
+    };
+    printf("%d -> ", node->value);
+    print(node->next);
+}
+
+
+/* Free the memory list */
 void liberate(Node * node) {
     if (node == NULL) {
         return;
@@ -47,6 +54,7 @@ void liberate(Node * node) {
 }
 
 
+/* Return the k-th value from a list */
 int get_kth_value(Node * node, int k) {
     if (node == NULL) {
         return 0;
@@ -59,6 +67,8 @@ int get_kth_value(Node * node, int k) {
     return get_kth_value(node->next, k - 1);
 }
 
+
+/* Compare two list */
 int is_equal(Node * node1, Node * node2) {
     if (node1 == NULL && node2 == NULL) {
         return 1;
@@ -79,12 +89,40 @@ int is_equal(Node * node1, Node * node2) {
     return is_equal(node1->next, node2->next);
 }
 
+
 int main(int argc, char const *argv[]) {
-    Node * list = NULL;
+    Node * list1 = NULL;
+    Node * list2 = NULL;
 
+    int ne = 100;
 
+    // Generate list 1
+    for (int i = 0; i < ne - 1; i++) {
+        add_element(&list1, i);
+    }
+    puts("List 1:");
+    print(list1);
 
-    print(list);
+    // Generate list 2
+    for (int i = 0; i < ne; i++) {
+        add_element(&list2, i);
+    }
+    puts("List 2:");
+    print(list2);
+
+    // compare lists
+    if(is_equal(list1, list2)) {
+        puts("The lists are equals");
+    } else {
+        puts("The lists are not equals");
+    }
+
+    //  Get the k-th element
+    printf("%d-th element for list 1 is: %d\n", 30, get_kth_value(list1, 30));
+    printf("%d-th element for list 2 is: %d\n", 40, get_kth_value(list2, 40));
+
+    liberate(list1);
+    liberate(list2);
 
     return 0;
 }
